ecddd87b06bad6e4c7e7f00b22fe4e8c529f645a
campidelli <bruno.campidelli@digio.com.au>
2023-06-20 12:15:40 +1200

    Adding TestContainers to the integration test (Adobe/S3Mock).

--- src/test/java/campidelli/file/storage/FileStorageApplicationTests.java
+++ src/test/java/campidelli/file/storage/FileStorageApplicationTests.java
@@ -1,36 +1,49 @@
 package campidelli.file.storage;
 
+import com.adobe.testing.s3mock.testcontainers.S3MockContainer;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @ActiveProfiles("test")
 @AutoConfigureWebTestClient(timeout = "36000")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 @Slf4j
 class FileStorageApplicationTests {
 
-	private final int serverPort;
-	private final TestRestTemplate restTemplate;
+	@LocalServerPort
+	private int serverPort;
 
 	@Autowired
-	public FileStorageApplicationTests(@Value("${local.server.port}") int serverPort,
-									   TestRestTemplate restTemplate) {
-		this.serverPort = serverPort;
-		this.restTemplate = restTemplate;
+	private TestRestTemplate restTemplate;
+
+	@Container
+	private static final S3MockContainer s3Mock = new S3MockContainer("2.12.2")
+			.withValidKmsKeys("arn:aws:kms:us-east-1:1234567890:key/valid-test-key-ref")
+			.withRetainFilesOnExit(true)
+			.withEnv("debug", "true");
+
+	@DynamicPropertySource
+	static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+		registry.add("aws.s3.endpoint", s3Mock::getHttpEndpoint);
 	}
 
 	@Test
